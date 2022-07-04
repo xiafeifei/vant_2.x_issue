3e383483472f313324c037797324959ef1fd2f60
xiafeifei <593670711@qq.com>
2022-07-04 20:54:30 +0800

    默认的

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -6,35 +6,62 @@
       check out the
       <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
     </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <van-form @submit="onSubmit">
+      <van-field
+        name="测试插槽"
+        label="插槽测试"
+      >
+        <template #input>
+          <baseInput
+            v-model="testInput"
+            placeholder="Enter your username"
+            :rules="[{ required: true, message: '该值不能为空' }]"/>
+        </template>
+      </van-field>
+      <van-field
+        v-model="username"
+        name="用户名"
+        label="用户名"
+        placeholder="用户名"
+        :rules="[{ required: true, message: '请填写用户名' }]"
+      />
+      <van-field
+        v-model="password"
+        type="password"
+        name="密码"
+        label="密码"
+        placeholder="密码"
+        :rules="[{ required: true, message: '请填写密码' }]"
+      />
+      <div style="margin: 16px;">
+        <van-button round block type="info" native-type="submit">提交</van-button>
+      </div>
+    </van-form>
   </div>
 </template>
 
 <script>
+import baseInput from './testComponents'
+
 export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  data () {
+    return {
+      testInput: '测试插槽内容',
+      username: '',
+      password: ''
+    }
+  },
+  components: {
+    baseInput
+  },
+  methods: {
+    onSubmit(values) {
+      this.$toast(`submit values: ${JSON.stringify(values)}`);
+    }
   }
 }
 </script>
@@ -44,14 +71,17 @@ export default {
 h3 {
   margin: 40px 0 0;
 }
+
 ul {
   list-style-type: none;
   padding: 0;
 }
+
 li {
   display: inline-block;
   margin: 0 10px;
 }
+
 a {
   color: #42b983;
 }
--- src/main.js
+++ src/main.js
@@ -1,6 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
+import { Button } from 'vant';
+import { Form } from 'vant';
+import { Field } from 'vant';
+import { Toast } from 'vant';
 
+Vue.use(Toast);
+Vue.use(Form);
+Vue.use(Field);
+Vue.use(Button);
 Vue.config.productionTip = false
 
 new Vue({
